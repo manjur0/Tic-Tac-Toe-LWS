5832494358baae510feef1ffdcc41a4db7348cb8
manjur0 <sfmanjur21@gmail.com>
2024-10-05 22:08:09 +0600

    Board and square copmonet basic ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-const Square = () => {
+const Square = ({ value }) => {
   return (
     <>
-      <button className="bg-white border border-gray-300 rounded-md  w-12 h-12 m-1 leading-9 text-lg ">X</button>
+      <button className="bg-white border border-gray-300 rounded-md  w-12 h-12 m-1 leading-9 text-lg ">
+        {value}
+      </button>
     </>
   );
 };
@@ -9,19 +11,19 @@ const Board = () => {
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={"1"} />
+        <Square value={"2"} />
+        <Square value={"3"} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={"4"} />
+        <Square value={"5"} />
+        <Square value={"6"} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={"7"} />
+        <Square value={"8"} />
+        <Square value={"9"} />
       </div>
     </>
   );
