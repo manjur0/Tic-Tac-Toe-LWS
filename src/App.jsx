f23fa59500af69ad45443c6d9e85a357c7adfbc2
manjur0 <sfmanjur21@gmail.com>
2024-10-05 21:18:28 +0600

    Board and square component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Square = () => {
+  return (
+    <>
+      <button className="bg-white border border-gray-300 rounded-md  w-12 h-12 m-1 leading-9 text-lg ">X</button>
+    </>
+  );
+};
+const Board = () => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default Board;
